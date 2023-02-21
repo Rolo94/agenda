8b1b7eef05653ce0716c79631e1948031cb78bcf
Rolando Suarez <rolandoasuarez94@gmail.com>
2023-02-21 16:58:06 -0300

    visualizacion de contactos

---
+++ contact/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='contact'),
+    path('view/<int:id>', views.view, name='contact_view')
+]
---
+++ contact/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Contact
+
+def index(request):
+    contacts = Contact.objects.filter(name__contains=request.GET.get('search', ''))
+    context = {
+        'contacts': contacts
+    }
+    return render(request, 'contact/index.html', context)
+
+def view(request, id):
+    contact = Contact.objects.get(id=id)
+    context = {
+        'contact': contact
+    }
+    return render(request, 'contact/detail.html', context)
